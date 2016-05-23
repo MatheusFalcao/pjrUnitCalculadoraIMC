8184c3ffe96c48b6816a50ad2ff1c6cde128a9a2
Matheus Falcão <maatheusfalcao@gmail.com>
2016-05-22 22:31:51 -0300

    Test Class iplemetation

--- src/control/CalculadoraIMC.java
+++ src/control/CalculadoraIMC.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 
 public class CalculadoraIMC {
 	
-	public String calcularIMC(double altura, double peso, String sexo, int idade){
+	public String calcularIMC(String sexo,double altura, double peso, int idade){
 		//calculo do IMC da pessoa
 		DecimalFormat df = new DecimalFormat("#.##");
 		altura *= altura;
@@ -13,229 +13,229 @@ public class CalculadoraIMC {
 		String c = "";
 		
 		if (idade < 20){
-			if (sexo == "feminino"){
+			if (sexo.equals("feminino")){
 				switch(idade){
 				case 2:
 					if (imc > 14 && imc < 15){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 15 && imc < 18){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 18 && imc < 19){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 20){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 4:
 					if (imc > 13.7 && imc < 14.1){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 14.2 && imc < 17){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 17 && imc < 18){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 18){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 6:
 					if (imc > 13.2 && imc < 13.9){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 13.9 && imc < 17){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 17 && imc < 18.9){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 18.9){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 8:
 					if (imc > 13.3 && imc < 14){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 14 && imc < 18.1){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 18.1 && imc < 19.6){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 19.6){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 10:
 					if (imc > 13.9 && imc < 14.4){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 14.4 && imc < 20){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 20 && imc < 23){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 23){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 12:
 					if (imc > 14.5 && imc < 15.1){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 15.1 && imc < 21.2){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 21.2 && imc < 25){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 25){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 14:
 					if (imc > 15.1 && imc < 16.1){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 16.1 && imc < 23.2){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 23.1 && imc < 27){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 27){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 16:
 					if (imc > 16.1 && imc < 17.5){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 17.5 && imc < 24.3){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 24.3 && imc < 30){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 30){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 18:
 					if (imc > 17.1 && imc < 18.1){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 18.1 && imc < 25.3){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 25.3 && imc < 30.1){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 30.1){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 20:
 					if (imc > 17.3 && imc < 18.5){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 18.5 && imc < 26.2){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 26.2 && imc < 31.2){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 31.2){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				}
-			} else if (sexo == "masculino"){
+			} else if (sexo.equals("masculino")){
 				switch(idade){
 				case 2:
 					if (imc > 14.5 && imc < 15){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 15 && imc < 18){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 18 && imc < 19.1){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 19.1){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 4:
 					if (imc > 13.8 && imc < 14.5){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 14.5 && imc < 17){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 17 && imc < 18){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 18){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 6:
 					if (imc > 13.8 && imc < 14){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 14 && imc < 17){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 17 && imc < 18.1){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 18.1){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 8:
 					if (imc > 13.5 && imc < 14.1){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 14.1 && imc < 18.9){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 18.9 && imc < 20){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 20){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 10:
 					if (imc > 13.9 && imc < 14.4){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 14.4 && imc < 19.1){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 19.1 && imc < 22){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 22){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 12:
 					if (imc > 14.5 && imc < 15.1){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 15.1 && imc < 21.2){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 21.2 && imc < 25){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 25){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 14:
 					if (imc > 15.8 && imc < 16.4){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 16.4 && imc < 22.4){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 22.4 && imc < 26){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 26){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 16:
 					if (imc > 16.9 && imc < 17.9){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 17.9 && imc < 24.1){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 24.1 && imc < 27.3){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 27.3){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 18:
 					if (imc > 18 && imc < 18.9){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 18.9 && imc < 25.5){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 25.5 && imc < 28.9){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 28.9){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				case 20:
 					if (imc > 18.9 && imc < 19.9){
-						c = "Baixo Peso";
+						c = "BAIXO PESO";
 					} else if (imc > 19.9 && imc < 27){
-						c = "Peso Normal";
+						c = "PESO NORMAL";
 					} else if (imc > 27 && imc < 30.2){
-						c = "Sobrepeso";
+						c = "SOBREPESO";
 					} else if (imc > 30.2){
-						c = "Obesidade";
+						c = "OBESIDADE";
 					}
 					break;
 				
@@ -244,52 +244,52 @@ public class CalculadoraIMC {
 		
 		if (idade >= 20 || idade <= 65){
 				if (imc < 16){
-					c = "Baixo peso muito grave";
+					c = "BAIXO PESO MUITO GRAVE";
 				} else if (imc > 16 && imc < 16.99){
-					c = "Baixo peso grave";
+					c = "BAIXO PESO GRAVE";
 				} else if (imc > 17 && imc < 18.49){
-					c = "Baixo peso";
+					c = "BAIXO PESO";
 				} else if (imc > 18.50 && imc < 24.99){
-					c = "Peso normal";
+					c = "PESO NORMAL";
 				} else if (imc > 24 && imc < 30){
-					c = "Sobrepeso";
+					c = "SOBREPESO";
 				} else if (imc > 30 && imc < 35){
-					c = "Obesidade grau I ";
+					c = "OBESIDADE GRAVE I ";
 				} else if (imc > 35 && imc < 40){
-					c = "Obesidade grau II ";
+					c = "OBESIDADE GRAVE II ";
 				} else if (imc > 40 ){				
-					c = "Obesidade grau III (obesidade mórbida)";
+					c = "OBESIDADE GRAVE III (OBESIDADE MÓRBIDA)";
 				}
 		}
 		
 		if (idade > 65){
-			if (sexo == "feminino"){
+			if (sexo.equals("feminino")){
 				if (imc < 21.9){
-					c = "Baixo peso";
+					c = "BAIXO PESO";
 				} else if (imc > 22 && imc < 27){
-					c = "Peso Normal";
+					c = "PESO NORMAL";
 				} else if (imc > 27 && imc < 32){
-					c = "Sobrepeso";
+					c = "SOBREPESO";
 				} else if (imc > 32 && imc < 37){
-					c = "Obesidade grau I";
+					c = "OBESIDADE GRAVE I";
 				} else if (imc > 37 && imc < 41){
-					c = "Obesidade grau II";
+					c = "OBESIDADE GRAVE II";
 				} else if (imc > 42){
-					c = "Obesidade grau III (obesidade mórbida)";
+					c = "OBESIDADE GRAVE III (OBESIDADE MÓRBIDA)";
 				}
-			} else if (sexo == "masculino"){
+			} else if (sexo.equals("masculino")){
 				if (imc < 21.9){
-					c = "Baixo peso";
+					c = "BAIXO PESO";
 				} else if (imc > 22 && imc < 27){
-					c = "Peso Normal";
+					c = "PESO NORMAL";
 				} else if (imc > 27 && imc < 30){
-					c = "Sobrepeso";
+					c = "SOBREPESO";
 				} else if (imc > 30 && imc < 35){
-					c = "Obesidade grau I";
+					c = "OBESIDADE GRAVE I";
 				} else if (imc > 35 && imc < 40){
-					c = "Obesidade grau II";
+					c = "OBESIDADE GRAVE II";
 				} else if (imc > 40){
-					c = "Obesidade grau III (obesidade mórbida)";
+					c = "OBESIDADE GRAVE III (OBESIDADE MÓRBIDA)";
 				}
 			}
 		}
--- src/teste/TesteCalc.java
+++ src/teste/TesteCalc.java
@@ -14,49 +14,507 @@ public class TesteCalc {
 	public void setup(){
 		c = new CalculadoraIMC();
 	}
-	
+
 	@Test
-	public void calcIMC(){
-		Assert.assertEquals(21.6, c.calcIMC(70, 1.80));	
+	public void TestAdultoBaixoPesoMuitoGrave(){
+		Assert.assertEquals("BAIXO PESO MUITO GRAVE", c.calcularIMC("feminino", 1.80, 50., 21));
+	}
+
+	@Test
+	public void TestAdultoBaixoPesoGrave(){
+		Assert.assertEquals("BAIXO PESO GRAVE", c.calcularIMC("feminino", 1.70, 48., 64));
 	}
 	
 	@Test
-	public void baixoMuitoGrave(){
-		Assert.assertEquals("Baixo peso muito grave" , c.calcCond(15));
+	public void TestAdultoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino",  1.70, 50., 64));
 	}
 	
 	@Test
-	public void baixoPesoGrave(){
-		Assert.assertEquals("Baixo peso grave" , c.calcCond(16.80));
+	public void TestAdultoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1.70, 54., 21));
 	}
 	
 	@Test
-	public void baixoPeso(){
-		Assert.assertEquals("Baixo peso" , c.calcCond(18));
+	public void TestAdultoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1.60, 65., 21));
+	}
+
+	@Test
+	public void TestAdultoObesidadeGrauUm(){
+		Assert.assertEquals("OBESIDADE GRAU I", c.calcularIMC("masculino", 1.60, 78., 64));
+	}
+
+	@Test
+	public void TestAdultoObesidadeGrauDois(){
+		Assert.assertEquals("OBESIDADE GRAU II ", c.calcularIMC("masculino", 1.70, 102., 64));
 	}
 	
 	@Test
-	public void pesoNormal(){
-		Assert.assertEquals("Peso normal" , c.calcCond(23));
+	public void TestAdultoObesidadeGrauTres(){
+		Assert.assertEquals("OBESIDADE GRAU II I", c.calcularIMC("masculino", 1.70, 120., 21));
+	}
+
+	@Test
+	public void TestIdosoFemininoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("feminino", 1.70, 60., 66));
 	}
 	
 	@Test
-	public void sobrePeso(){
-		Assert.assertEquals("Sobrepeso" , c.calcCond(29));
+	public void TestIdosoFemininoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("feminino", 1.70, 65., 66));
 	}
 	
 	@Test
-	public void obesidadeI(){
-		Assert.assertEquals("Obesidade grau I " , c.calcCond(34));
+	public void TestIdosoFemininoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1.60, 70., 66));
 	}
 	
 	@Test
-	public void obesidadeII(){
-		Assert.assertEquals("Obesidade grau II " , c.calcCond(39));
+	public void TestIdosoFemininoObesidadeGrauUm(){
+		Assert.assertEquals("OBESIDADE GRAU I", c.calcularIMC("feminino", 1.60, 84., 66));
+	}	
+	
+	@Test
+	public void TestIdosoFemininoObesidadeGrauDois(){
+		Assert.assertEquals("OBESIDADE GRAU II ", c.calcularIMC("feminino", 1.60, 97., 66));
 	}
 	
 	@Test
-	public void obesidadeMorbida(){
-		Assert.assertEquals("Obesidade grau III (obesidade mórbida)" , c.calcCond(41));
+	public void TestIdosoFemininoObesidadeGrauTres(){
+		Assert.assertEquals("OBESIDADE GRAU II I", c.calcularIMC("feminino", 1.70, 123., 66));
+	}	
+	
+	@Test
+	public void TestIdosoMasculinoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino", 1.60, 53., 66));
 	}
+	
+	@Test
+	public void TestIdosoMasculinoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1.60, 58., 66));
+	}
+	
+	@Test
+	public void TestIdosoMasculinoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("masculino", 1.60, 70., 66));
+	}
+	
+	@Test
+	public void TestIdosoMasculinoObesidadeGrauUm(){
+		Assert.assertEquals("OBESIDADE GRAU I", c.calcularIMC("masculino", 1.70, 90., 66));
+	}	
+	
+	@Test
+	public void TestIdosoMasculinoObesidadeGrauDois(){
+		Assert.assertEquals("OBESIDADE GRAU II ", c.calcularIMC("masculino", 1.70, 104., 66));
+	}
+	
+	@Test
+	public void TestIdosoMasculinoObesidadeGrauTres(){
+		Assert.assertEquals("OBESIDADE GRAU II I", c.calcularIMC("masculino", 1.70, 120., 66));
+	}	
+
+	@Test
+	public void TestCriancaDoisAnosFemininoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("feminino", 1., 14., 2));
+	}
+	
+	@Test
+	public void TestCriancaDoisAnosFemininoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("feminino", 1., 15., 2));
+	}
+	
+	@Test
+	public void TestCriancaDoisAnosFemininoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1., 18., 2));
+	}
+	
+	@Test
+	public void TestCriancaDoisAnosFemininoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("feminino", 1., 21., 2));
+	}
+
+	@Test
+	public void TestCriancaQuatroAnosFemininoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("feminino", 1.05, 15.1, 4));
+	}
+	
+	@Test
+	public void TestCriancaQuatroAnosFemininoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("feminino", 1.05, 16., 4));
+	}
+	
+	@Test
+	public void TestCriancaQuatroAnosFemininoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1.05, 19., 4));
+	}
+	
+	@Test
+	public void TestCriancaQuatroAnosFemininoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("feminino", 1.05, 20., 4));
+	}
+
+	@Test
+	public void TestCriancaSeisAnosFemininoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("feminino", 1.15, 18., 6));
+	}
+	
+	@Test
+	public void TestCriancaSeisAnosFemininoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("feminino", 1.15, 19., 6));
+	}
+	
+	@Test
+	public void TestCriancaSeisAnosFemininoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1.15, 23., 6));
+	}
+	
+	@Test
+	public void TestCriancaSeisAnosFemininoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("feminino", 1.15, 26., 6));
+	}
+	
+	@Test
+	public void TestCriancaOitoAnosFemininoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("feminino", 1.25, 21., 8));
+	}
+	
+	@Test
+	public void TestCriancaOitoAnosFemininoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("feminino", 1.25, 22., 8));
+	}
+	
+	@Test
+	public void TestCriancaOitoAnosFemininoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1.25, 29., 8));
+	}
+	
+	@Test
+	public void TestCriancaOitoAnosFemininoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("feminino", 1.25, 31., 8));
+	}
+	
+	@Test
+	public void TestCriancaDezAnosFemininoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("feminino", 1.40, 27.5, 10));
+	}
+	
+	@Test
+	public void TestCriancaDezAnosFemininoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("feminino", 1.40, 29., 10));
+	}
+	
+	@Test
+	public void TestCriancaDezAnosFemininoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1.40, 40., 10));
+	}
+	
+	@Test
+	public void TestCriancaDezAnosFemininoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("feminino", 1.40, 47., 10));
+	}
+	
+	@Test
+	public void TestCriancaDozeAnosFemininoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("feminino", 1.45, 30.5, 12));
+	}
+	
+	@Test
+	public void TestCriancaDozeAnosFemininoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("feminino", 1.45, 33., 12));
+	}
+	
+	@Test
+	public void TestCriancaDozeAnosFemininoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1.45, 45., 12));
+	}
+	
+	@Test
+	public void TestCriancaDozeAnosFemininoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("feminino", 1.45, 54., 12));
+	}
+	
+	@Test
+	public void TestCriancaQuatorzeAnosFemininoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("feminino", 1.50, 35., 14));
+	}
+	
+	@Test
+	public void TestCriancaQuatorzeAnosFemininoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("feminino", 1.50, 36.5, 14));
+	}
+	
+	@Test
+	public void TestCriancaQuatorzeAnosFemininoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1.50, 53., 14));
+	}
+	
+	@Test
+	public void TestCriancaQuatorzeAnosFemininoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("feminino", 1.50, 61., 14));
+	}
+	
+	@Test
+	public void TestCriancaDezesseisAnosFemininoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("feminino", 1.55, 39., 16));
+	}
+	
+	@Test
+	public void TestCriancaDezesseisAnosFemininoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("feminino", 1.55, 43., 16));
+	}
+	
+	@Test
+	public void TestCriancaDezesseisAnosFemininoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1.55, 59., 16));
+	}
+	
+	@Test
+	public void TestCriancaDezesseisAnosFemininoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("feminino", 1.55, 73., 16));
+	}
+	
+	@Test
+	public void TestCriancaDezoitoAnosFemininoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("feminino", 1.60, 45., 18));
+	}
+	
+	@Test
+	public void TestCriancaDezoitoAnosFemininoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("feminino", 1.60, 49., 18));
+	}
+	
+	@Test
+	public void TestCriancaDezoitoAnosFemininoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1.60, 65., 18));
+	}
+	
+	@Test
+	public void TestCriancaDezoitoAnosFemininoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("feminino", 1.65, 87., 18));
+	}
+	
+	@Test
+	public void TestCriancaVinteAnosFemininoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("feminino", 1.70, 50., 20));
+	}
+	
+	@Test
+	public void TestCriancaVinteAnosFemininoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("feminino", 1.70, 54., 20));
+	}
+	
+	@Test
+	public void TestCriancaVinteAnosFemininoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("feminino", 1.70, 77., 20));
+	}
+	
+	@Test
+	public void TestCriancaVinteAnosFemininoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("feminino", 1.70, 92., 20));
+	}
+
+
+	//---------MASCULINO CRIANÇA-----------//
+	@Test
+	public void TestCriancaDoisAnosMasculinoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino", 1., 14.5, 2));
+	}
+	
+	@Test
+	public void TestCriancaDoisAnosMasculinoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1., 15., 2));
+	}
+	
+	@Test
+	public void TestCriancaDoisAnosMasculinoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("masculino", 1., 18., 2));
+	}
+	
+	@Test
+	public void TestCriancaDoisAnosMasculinoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("masculino", 1., 21., 2));
+	}
+
+	@Test
+	public void TestCriancaQuatroAnosMasculinoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino", 1.05, 15.5, 4));
+	}
+	
+	@Test
+	public void TestCriancaQuatroAnosMasculinoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1.05, 16., 4));
+	}
+	
+	@Test
+	public void TestCriancaQuatroAnosMasculinoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("masculino", 1.05, 19., 4));
+	}
+	
+	@Test
+	public void TestCriancaQuatroAnosMasculinoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("masculino", 1.05, 20., 4));
+	}
+
+	@Test
+	public void TestCriancaSeisAnosMasculinoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino", 1.15, 18.5, 6));
+	}
+	
+	@Test
+	public void TestCriancaSeisAnosMasculinoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1.15, 19., 6));
+	}
+	
+	@Test
+	public void TestCriancaSeisAnosMasculinoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("masculino", 1.15, 23., 6));
+	}
+	
+	@Test
+	public void TestCriancaSeisAnosMasculinoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("masculino", 1.15, 26., 6));
+	}
+	
+	@Test
+	public void TestCriancaOitoAnosMasculinoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino", 1.25, 21.5, 8));
+	}
+	
+	@Test
+	public void TestCriancaOitoAnosMasculinoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1.25, 23., 8));
+	}
+	
+	@Test
+	public void TestCriancaOitoAnosMasculinoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("masculino", 1.25, 30., 8));
+	}
+	
+	@Test
+	public void TestCriancaOitoAnosMasculinoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("masculino", 1.25, 32., 8));
+	}
+	
+	@Test
+	public void TestCriancaDezAnosMasculinoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino", 1.40, 27.5, 10));
+	}
+	
+	@Test
+	public void TestCriancaDezAnosMasculinoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1.40, 29., 10));
+	}
+	
+	@Test
+	public void TestCriancaDezAnosMasculinoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("masculino", 1.40, 40., 10));
+	}
+	
+	@Test
+	public void TestCriancaDezAnosMasculinoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("masculino", 1.40, 47., 10));
+	}
+	
+	@Test
+	public void TestCriancaDozeAnosMasculinoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino", 1.45, 30.5, 12));
+	}
+	
+	@Test
+	public void TestCriancaDozeAnosMasculinoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1.45, 33., 12));
+	}
+	
+	@Test
+	public void TestCriancaDozeAnosMasculinoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("masculino", 1.45, 45., 12));
+	}
+	
+	@Test
+	public void TestCriancaDozeAnosMasculinoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("masculino", 1.45, 54., 12));
+	}
+	
+	@Test
+	public void TestCriancaQuatorzeAnosMasculinoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino", 1.50, 36., 14));
+	}
+	
+	@Test
+	public void TestCriancaQuatorzeAnosMasculinoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1.50, 37., 14));
+	}
+	
+	@Test
+	public void TestCriancaQuatorzeAnosMasculinoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("masculino", 1.50, 53., 14));
+	}
+	
+	@Test
+	public void TestCriancaQuatorzeAnosMasculinoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("masculino", 1.50, 61., 14));
+	}
+	
+	@Test
+	public void TestCriancaDezesseisAnosMasculinoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino", 1.55, 41., 16));
+	}
+	
+	@Test
+	public void TestCriancaDezesseisAnosMasculinoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1.55, 43., 16));
+	}
+	
+	@Test
+	public void TestCriancaDezesseisAnosMasculinoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("masculino", 1.55, 59., 16));
+	}
+	
+	@Test
+	public void TestCriancaDezesseisAnosMasculinoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("masculino", 1.55, 73., 16));
+	}
+	
+	@Test
+	public void TestCriancaDezoitoAnosMasculinoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino", 1.60, 48., 18));
+	}
+	
+	@Test
+	public void TestCriancaDezoitoAnosMasculinoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1.60, 49., 18));
+	}
+	
+	@Test
+	public void TestCriancaDezoitoAnosMasculinoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("masculino", 1.60, 66., 18));
+	}
+	
+	@Test
+	public void TestCriancaDezoitoAnosMasculinoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("masculino", 1.60, 81., 18));
+	}
+	
+	@Test
+	public void TestCriancaVinteAnosMasculinoBaixoPeso(){
+		Assert.assertEquals("BAIXO PESO", c.calcularIMC("masculino", 1.70, 55., 20));
+	}
+	
+	@Test
+	public void TestCriancaVinteAnosMasculinoPesoNormal(){
+		Assert.assertEquals("PESO NORMAL", c.calcularIMC("masculino", 1.70, 58., 20));
+	}
+	
+	@Test
+	public void TestCriancaVinteAnosMasculinoSobrepeso(){
+		Assert.assertEquals("SOBREPESO", c.calcularIMC("masculino", 1.70, 80., 20));
+	}
+	
+	@Test
+	public void TestCriancaVinteAnosMasculinoObesidade(){
+		Assert.assertEquals("OBESIDADE", c.calcularIMC("masculino", 1.70, 92., 20));
+	}
+
 }
